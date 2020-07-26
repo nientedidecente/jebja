99a623d3598636cf42ebe1b7860357ec03d11d5a
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-07-26 17:45:58 +0100

    made buoy circles

--- src/jebja/entities/Buoy.hx
+++ src/jebja/entities/Buoy.hx
@@ -1,27 +1,49 @@
 package jebja.entities;
 
+import h2d.Graphics;
 import h2d.Bitmap;
 import h2d.col.Point;
 import h2d.Object;
 import jebja.config.Colours;
 import jebja.libs.Randomizer;
-import h2d.Tile;
-import differ.shapes.Circle;
 
-class Buoy extends Bitmap {
+class Buoy {
 	var size:Int;
 
+	public var texture:Graphics;
+	public var x(get, set):Float;
+	public var y(get, set):Float;
+
+	public function get_x() {
+		return this.texture.x;
+	}
+
+	public function set_x(x) {
+		return this.texture.x = x;
+	}
+
+	public function get_y() {
+		return this.texture.y;
+	}
+
+	public function set_y(y) {
+		return this.texture.y = y;
+	}
+
 	public function new(parent:Object) {
-		this.size = Randomizer.int(10, 80);
-		var tile = Tile.fromColor(Colours.BUOY, size, size);
-		tile = tile.center();
-		super(tile, parent);
-		visible = false;
+		this.size = Randomizer.int(5, 10);
+
+		var cirle = new Graphics(parent);
+		cirle.beginFill(Colours.BUOY);
+		cirle.drawCircle(0, 0, size);
+		cirle.endFill();
+
+		this.texture = cirle;
 	}
 
 	public function update(player:Player) {
 		var parentPos = new Point(player.x, player.y);
 		var me = new Point(this.x, this.y);
-		this.visible = parentPos.distance(me) < 900 + this.size / 2;
+		this.texture.visible = parentPos.distance(me) < 900 + this.size / 2;
 	}
 }
