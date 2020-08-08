282afd277b0407b97fe3cefc153e4198bf173c0e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-08-08 13:06:15 +0100

    made target to arrow rotate along side the slope of the joining line

--- src/jebja/entities/TargetBuoy.hx
+++ src/jebja/entities/TargetBuoy.hx
@@ -6,6 +6,7 @@ import h2d.Bitmap;
 import h2d.col.Point;
 import h2d.Object;
 import jebja.config.Colours;
+import hxd.Math;
 
 class TargetBuoy extends Buoy {
 	public var indicator:Bitmap;
@@ -14,6 +15,7 @@ class TargetBuoy extends Buoy {
 		super(parent, Colours.BUOY_LIGHT);
 
 		indicator = new Bitmap(Atlas.instance.getRes('target').toTile(), parent);
+		indicator.scale(.5);
 	}
 
 	override public function update(player:Player) {
@@ -22,11 +24,12 @@ class TargetBuoy extends Buoy {
 		var distance = parentPos.distance(me);
 		var inView = distance < 900 + this.size / 2;
 
-		var pos = Geom.pointOnLine(player.x, player.y, x, y, distance, 200);
+		var pos = Geom.pointOnLine(player.x, player.y, x, y, distance, 400);
 		this.indicator.x = pos.x;
 		this.indicator.y = pos.y;
 
 		this.texture.visible = inView;
-		this.indicator.visible = !inView;
+		this.indicator.visible = distance > 500;
+		this.indicator.rotation = (Math.PI / 2) + (player.y - y) / (player.x - x);
 	}
 }
